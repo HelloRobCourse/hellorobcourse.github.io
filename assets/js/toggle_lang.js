2e4bebdaf20afbb4b042c2287b686b0900ebcb76
Jana Pavlasek <jbpavlasek@gmail.com>
2023-08-21 21:21:59 -0400

    Add Python toggle for Project 1

---
+++ assets/js/toggle_lang.js
@@ -0,0 +1,27 @@
+// Code to toggle whether Python or C++ is displayed.
+document.getElementById('lang-toggle').onchange = function() {
+
+    var cppElements = document.getElementsByClassName('cpp-content');
+    var pythonElements = document.getElementsByClassName('python-content');
+    var i;
+
+    for (i = 0; i < cppElements.length; i++) {
+        cppElements[i].style.display = this.checked ? 'none' : 'inline-block';
+    }
+
+    for (i = 0; i < pythonElements.length; i++) {
+        pythonElements[i].style.display = this.checked ? 'inline-block' : 'none';
+    }
+
+    // Add active labels to the labels.
+    var cppTextElement = document.getElementById('cpp-label');
+    var pythonTextElement = document.getElementById('python-label');
+    if(this.checked) {
+        pythonTextElement.classList.add('active-lang');
+        cppTextElement.classList.remove('active-lang');
+    } else {
+        cppTextElement.classList.add('active-lang');
+        pythonTextElement.classList.remove('active-lang');
+    }
+
+}
